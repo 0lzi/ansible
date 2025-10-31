a98eb439555481e1f508667289f2e05fa855ea52
Oliver Kelly <foolishhim@gmail.com>
2025-10-31 09:00:07 +0000

    update notes

--- roles/openbao_server_init/templates/openbao-backup.sh
+++ roles/openbao_server_init/templates/openbao-backup.sh
@@ -3,5 +3,6 @@
 export BAO_ADDR=$(/usr/bin/bao operator members list | grep true | awk '{print $2}')
 export BAO_TOKEN=$(cat /root/.vault-token)
 
+# Probably need to move this location as it gets nuked after a bao-cleanup
 /usr/bin/bao operator raft snapshot save /opt/openbao/data/raft-snapshot.snap
 
